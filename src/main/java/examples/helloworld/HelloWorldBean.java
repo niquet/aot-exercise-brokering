6f0bdcea5f4c556f5af372958a5c97e578d542ac
Maximilian Niquet <maximilian.niquet@gmail.com>
2021-05-18 13:25:09 +0200

    Examples aus Manual hinzugefügt

---
+++ src/main/java/examples/helloworld/HelloWorldBean.java
@@ -0,0 +1,10 @@
+package examples.helloworld;
+import de.dailab.jiactng.agentcore.AbstractAgentBean;
+
+public class HelloWorldBean extends AbstractAgentBean {
+
+    public void execute() {
+        System.out.println("Hello World!");
+    }
+
+}
