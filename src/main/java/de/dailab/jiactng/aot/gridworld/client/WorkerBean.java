2267349cb33e7b8c6c39c6dffd03064b2cf9b3a9
riebian-ht <h.tannenberg@tu-berlin.de>
2021-05-20 12:24:58 +0200

    Branch zu Henri

--- src/main/java/de/dailab/jiactng/aot/gridworld/client/WorkerBean.java
+++ src/main/java/de/dailab/jiactng/aot/gridworld/client/WorkerBean.java
@@ -127,7 +127,7 @@ public class WorkerBean extends AbstractAgentBean {
 				move.action = getNextMove(position, firstOrder.position);
 				move.gameId = gameId;
 				move.workerId = workerIdForServer;
-				System.out.println("WORKERIDFORSERVER " + workerIdForServer);
+				//System.out.println("WORKERIDFORSERVER " + workerIdForServer);
 
 				sendMessage(orderToAddress.get(firstOrder), move);
 
@@ -354,6 +354,49 @@ public class WorkerBean extends AbstractAgentBean {
 		if (action == WorkerAction.SOUTH) position = new Position(position.x, position.y + 1);
 		if (action == WorkerAction.WEST)  position = new Position(position.x - 1, position.y);
 		if (action == WorkerAction.EAST)  position = new Position(position.x + 1, position.y);
+	}
+
+	/**
+	 * This function is used to check whether an order should be accepted by the worker
+	 * Especially it returns the index of the position where the worker should put the order
+	 * If bestIndex == -1 then no feasible position to insert the order is found and the worker
+	 * should not accept the order since it is not able to finish all orders within the deadline
+	 */
+	public int checkOrder(Order order) {
+
+		int bestIndex = -1;
+		int minDuration = Integer.MAX_VALUE;
+
+		for (int index = 0; index <= orders.size(); index++) {
+			ArrayList<Order> temporaryOrders = orders;
+			temporaryOrders.add(index, order);
+
+			int currentDuration = calculateDuration(temporaryOrders);
+			if(currentDuration == -1)
+				continue;
+			if(currentDuration < minDuration) {
+				minDuration = currentDuration;
+				bestIndex = index;
+			}
+
+		}
+
+		return bestIndex;
+	}
+
+	private int calculateDuration(ArrayList<Order> orders) {
+		Position currentPosition = this.position;
+		int currentDuration = 0;
+
+		for(Order currentOrder: orders) {
+
+			currentDuration += currentOrder.position.distance(currentPosition);
+			currentDuration += 1;
+			currentPosition = currentOrder.position;
+			if (currentDuration > currentOrder.deadline)
+				return -1;
+		}
+		return currentDuration;
 	}
 
 }
--- src/main/java/de/dailab/jiactng/aot/gridworld/model/WorkerEstimate.java
+++ src/main/java/de/dailab/jiactng/aot/gridworld/model/WorkerEstimate.java
@@ -18,6 +18,49 @@ public class WorkerEstimate {
 
     }
 
+    /**
+     * This function is used to check whether an order should be accepted by the worker
+     * Especially it returns the index of the position where the worker should put the order
+     * If bestIndex == -1 then no feasible position to insert the order is found and the worker
+     * should not accept the order since it is not able to finish all orders within the deadline
+     */
+    public int checkOrder(Order order) {
+
+        int bestIndex = -1;
+        int minDuration = Integer.MAX_VALUE;
+
+        for (int index = 0; index <= orders.size(); index++) {
+            ArrayList<Order> temporaryOrders = orders;
+            temporaryOrders.add(index, order);
+
+            int currentDuration = calculateDuration(temporaryOrders);
+            if(currentDuration == -1)
+                continue;
+            if(currentDuration < minDuration) {
+                minDuration = currentDuration;
+                bestIndex = index;
+            }
+
+        }
+
+        return bestIndex;
+    }
+
+    private int calculateDuration(ArrayList<Order> orders) {
+        Position currentPosition = this.position;
+        int currentDuration = 0;
+
+        for(Order currentOrder: orders) {
+
+            currentDuration += currentOrder.position.distance(currentPosition);
+            currentDuration += 1;
+            currentPosition = currentOrder.position;
+            if (currentDuration > currentOrder.deadline)
+                return -1;
+        }
+        return currentDuration;
+    }
+
     public void assignOrder(Order order) {
 
         this.orders.add(order);
